5fd2a6a578c1986dc2d880f308c6dd42b50d3fe8
lykmapipo <lallyelias87@gmail.com>
2018-04-04 16:51:49 +0300

    add apidoc grunt task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,50 +1,62 @@
 'use strict';
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-    // add grunt tasks.
-    grunt.loadNpmTasks('grunt-mocha-test');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-watch');
+  // add grunt tasks.
+  grunt.loadNpmTasks('grunt-mocha-test');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-apidoc');
 
-    grunt.initConfig({
-        // Configure a mochaTest task
-        mochaTest: {
-            test: {
-                options: {
-                    reporter: 'spec',
-                    timeout: 20000
-                },
-                src: ['test/**/*.js']
-            }
-        },
-        jshint: {
-            options: {
-                reporter: require('jshint-stylish'),
-                jshintrc: '.jshintrc'
-            },
-            all: [
-                'Gruntfile.js',
-                'index.js',
-                'lib/**/*.js',
-                'test/**/*.js'
-            ]
-        },
-        watch: {
-            all: {
-                files: [
-                    'Gruntfile.js',
-                    'index.js',
-                    'lib/**/*.js',
-                    'test/**/*.js'
-                ],
-                tasks: ['default']
-            }
+  grunt.initConfig({
+    apidoc: {
+      api: {
+        src: 'lib/routers/',
+        dest: 'docs/',
+        options: {
+          debug: true,
+          includeFilters: ['.*\\.js$'],
+          excludeFilters: ['node_modules/']
         }
-    });
+      }
+    },
+    mochaTest: {
+      test: {
+        options: {
+          reporter: 'spec',
+          timeout: 20000
+        },
+        src: ['test/**/*.js']
+      }
+    },
+    jshint: {
+      options: {
+        reporter: require('jshint-stylish'),
+        jshintrc: '.jshintrc'
+      },
+      all: [
+        'Gruntfile.js',
+        'index.js',
+        'lib/**/*.js',
+        'test/**/*.js'
+      ]
+    },
+    watch: {
+      all: {
+        files: [
+          'Gruntfile.js',
+          'index.js',
+          'lib/**/*.js',
+          'test/**/*.js'
+        ],
+        tasks: ['default']
+      }
+    }
+  });
 
-    //custom tasks
-    grunt.registerTask('default', ['jshint', 'mochaTest', 'watch']);
-    grunt.registerTask('test', ['jshint', 'mochaTest']);
+  //custom tasks
+  grunt.registerTask('default', ['jshint', 'mochaTest', 'watch']);
+  grunt.registerTask('test', ['jshint', 'mochaTest']);
+  grunt.registerTask('doc', ['jshint', 'apidoc:api']);
 
 };
